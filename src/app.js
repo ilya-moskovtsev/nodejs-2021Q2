9c0ca19ea257f4445320567fe8cd342d9782cde7
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2021-05-13 01:09:05 +0300

    task 5.1 add express middleware to log invoked methods with arguments

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+
 import { userRoutes } from './routes/user';
 import { groupRoutes } from './routes/group';
 import db from './loaders/database';
@@ -7,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
+app.use(morgan('combined'));
 app.use(express.json());
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/groups', groupRoutes);
